381833fd7f8f9d1a36cd1fb1c2bd4db31dd6e0e0
Tomas Cejka <cejkat@cesnet.cz>
2017-03-05 23:17:16 +0100

    nagios: free_memory: replace free with available mem

--- nagios/nagios-client/scripts/check_free_memory.sh
+++ nagios/nagios-client/scripts/check_free_memory.sh
@@ -3,7 +3,7 @@
 #If it is less than 10% of free memory it returns 2 as critical.
 
 TOTAL=$(cat /proc/meminfo | grep MemTotal | tr -s ' ' | cut -d' ' -f2)
-FREE=$(cat /proc/meminfo | grep MemFree | tr -s ' ' | cut -d' ' -f2)
+FREE=$(cat /proc/meminfo | grep MemAvailable | tr -s ' ' | cut -d' ' -f2)
 
 #PERCENT of total memory used
 PERCENT=$( echo "scale=2; (($TOTAL-$FREE)/$TOTAL)*100" | bc | cut -d'.' -f1)
