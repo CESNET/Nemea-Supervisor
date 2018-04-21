e4e30182e4aa6be0d9017fb159956901f225c86c
asd <asdasd>
2018-04-21 18:16:39 +0200

    copied real configuration of STaaS here

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 THIS_DIR="$(dirname $0)"
 
 sysrepoctl --install --yang "${THIS_DIR}/yang/nemea.yang"
-sysrepocfg --import="${THIS_DIR}/yang/nemea-staas-startup-config.data.json" \
+sysrepocfg --import="${THIS_DIR}/yang/data/nemea-staas-startup-config.data.json" \
             --format=json --datastore startup nemea
