7e2baf488618e36570e76ee0f9d695f981e4dae0
asd <asdasd>
2018-04-24 10:04:15 +0200

    bugfixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 THIS_DIR="$(dirname $0)"
 
 sysrepoctl --install --yang "${THIS_DIR}/yang/nemea.yang"
-sysrepocfg --import="${THIS_DIR}/yang/data/nemea-staas-startup-config.data.json" \
-            --format=json --datastore startup nemea
+sysrepocfg --import="${THIS_DIR}/yang/data/nemea-staas-startup-config.data.xml" \
+            --format=xml --datastore startup nemea
