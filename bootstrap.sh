7fb38ed4d2d1a1b57acbfdac49bd1640f47fe637
asd <asdasd>
2018-05-25 20:17:40 +0200

    make bootstrap safer for sudo and non-sudo environment

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,18 @@ THIS_DIR="$(dirname $0)"
 
 tools=( sysrepoctl sysrepocfg )
 for tool in ${tools[@]}; do
-    command $tool &>/dev/null
+    command -v $tool &>/dev/null
     if [ $? -ne 0 ]; then
         echo "${tool} from sysrepo project is missing, please install sysrepo before you continue!"
         exit 1
     fi
 done
 
-sudo sysrepoctl --install --yang "${THIS_DIR}/yang/nemea.yang"
-sysrepocfg --import="${THIS_DIR}/yang/data/nemea-staas-startup-config.data.xml" \
-            --format=xml --datastore startup nemea
+if [[ "$(whoami)" != "root" ]]; then
+    sudo sysrepoctl --install --yang "${THIS_DIR}/yang/nemea.yang"
+else
+    sysrepoctl --install --yang "${THIS_DIR}/yang/nemea.yang"
+fi
+if [[ $? -ne 0 ]]; then  exit 1; fi
 
 echo "Successfully bootstraped."
